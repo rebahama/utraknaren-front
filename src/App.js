52c613629a4ca895d10aa36c899cf8ff93f08ea7
rebahama <wiri@hotmail.se>
2023-04-06 12:08:01 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import CreateAccount from "./auth/CreateAccount";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/createaccount" element={<CreateAccount />} />
