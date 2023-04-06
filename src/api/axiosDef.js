c78940f3bc73f7a12de99bbd22bb5d4d0f1544cc
rebahama <wiri@hotmail.se>
2023-04-06 12:27:54 +0000

    add axios

---
+++ src/api/axiosDef.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://utraknaren-front.herokuapp.com/"; 
+axios.defaults.headers.post["Content-Type"] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
