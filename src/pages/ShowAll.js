af26d575cdfce6640565b0c1b2cb4d6571538173
rebahama <wiri@hotmail.se>
2023-04-06 12:56:53 +0000

    add get array

--- src/pages/ShowAll.js
+++ src/pages/ShowAll.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ShowAll = () => {
-  return (
-    <div>ShowAll</div>
-  )
-}
+  const [calculate, setCalculate] = useState({ results: [] });
 
-export default ShowAll
+  useEffect(() => {
+    const handleData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://utraknaren-drf.herokuapp.com/posts/"
+        );
+
+        setCalculate(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleData();
+  }, []);
+
+  return (
+    <div>
+      {calculate.results.map((calculate) => {
+        return <div key={calculate.id}>
+            {calculate.name}
+            {calculate.content}
+            </div>;
+      })}
+    </div>
+  );
+};
+
+export default ShowAll;
