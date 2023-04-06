b56a5da6574af323132d165daf38dc8c5e8e2776
rebahama <wiri@hotmail.se>
2023-04-06 11:34:29 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { Button } from 'react-bootstrap';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p> hello </p>
+    <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
