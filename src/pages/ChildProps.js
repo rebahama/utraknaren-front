8f056ff60bacd6f6e90917c619a96810c5146ebb
rebahama <wiri@hotmail.se>
2023-04-24 16:05:42 +0000

    style card

--- src/pages/ChildProps.js
+++ src/pages/ChildProps.js
@@ -1,27 +1,42 @@
 import React from "react";
+import styles from "../styles/ChildPros.module.css";
 import { Card, Col, Container, Row } from "react-bootstrap";
 
 const ChildProps = (props) => {
   const { id, title, calculate_name, name, created_at, updated_at } = props;
   return (
-    <Container>
-      <Row>
-        <Col md={12}>
-          <Card>
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                ID: {id}
-              </Card.Subtitle>
-              <Card.Text>Name: {name}</Card.Text>
-              <Card.Text>Calculate Name: {calculate_name}</Card.Text>
-              <Card.Text>Created At: {created_at}</Card.Text>
-              <Card.Text>Updated At: {updated_at}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className={styles.backgroundContainer}>
+      <Container>
+        <Row>
+          <Col md={12} className={styles.marginContainer}>
+            <Card className={styles["custom-card"]}>
+              <Card.Body>
+                <div className={styles.FlexContainer}>
+                  <Card.Title className={styles["custom-card-title"]}>
+                    {title}
+                  </Card.Title>
+                  <Card.Subtitle className={styles["custom-card-subtitle"]}>
+                    ID: {id}
+                  </Card.Subtitle>
+                  <Card.Text className={styles["custom-card-text"]}>
+                    Name: {name}
+                  </Card.Text>
+                  <Card.Text className={styles["custom-card-text"]}>
+                    Calculate Name: {calculate_name}
+                  </Card.Text>
+                  <Card.Text className={styles["custom-card-text"]}>
+                    Created At: {created_at}
+                  </Card.Text>
+                  <Card.Text className={styles["custom-card-text"]}>
+                    Updated At: {updated_at}
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
